db0b5f386deddbf334c58481e635edac20f41ea5
farruxuzbekistan <69811272+farruxuzbekistan@users.noreply.github.com>
2024-09-23 12:27:05 +0500

    experience changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import meta from "./company/ftargetit.png";
+import shopify from "./company/ftarget.png";
+import starbucks from "./company/faddline.png";
+import tesla from "./company/fhamkorbank.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
